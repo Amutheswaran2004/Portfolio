197361f99c8fa61e5953ee6ecf33e9c70722283d
Amutheswaran2004 <amudheswaran@gmail.com>
2025-05-12 10:01:06 +0530

    Updates

--- backend_sanity/schemaTypes/index.js
+++ backend_sanity/schemaTypes/index.js
@@ -1,2 +1,20 @@
-import testimonials from "./testimonials"
-export const schemaTypes = [testimonials]
+import testimonials from './testimonials'
+import abouts from './abouts'
+import brands from './brands'
+import contact from './contact'
+import experiences from './experiences'
+import workExperience from './workExperience'
+import works from './works'
+import skills from './skills'
+
+export const schemaTypes = [
+  works,
+  testimonials,
+  brands,
+  abouts,
+  skills,
+  workExperience,
+  experiences,
+  contact,
+  /* Your types here! */
+]
